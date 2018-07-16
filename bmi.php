073108e2346251057c39b9eb5015df9cd4ab3e43
Veeno09 <nwasm30@gmail.com>
2018-07-16 22:17:19 +0200

    tyr

--- bmi.php
+++ bmi.php
@@ -1,43 +1,38 @@
 <?php
+include "index.php";
 
-$waga=$_POST["waga"]; //waga w kg
-$wzrost=$_POST["wzrost"]; // wzrost w cm
-//sprawdzenie  czy dane są poprawne
-if (preg_match("@[0-9]@",$_POST["waga"]))  {
-} else {
-  echo "Nieprawidłowe dane!";
-  die();
-}
-if (preg_match("@[0-9]@",$_POST["wzrost"]))  {
-} else {
-  echo "Nieprawidłowe dane!";
-  die();
-}
-$BMI=$waga/($wzrost/100*$wzrost/100); //obliczenie wskaźnika BMI
-//sprawdzanie przedziału obliczonego wskaźnika
- if($BMI < 15) {
-    echo "Wskaźnik BMI wynosi: ".round($BMI)."<br>"."Anoreksja";
+
+function calculateBMI() {
+  $waga=$_POST["waga"]; //waga w kg
+  $wzrost=$_POST["wzrost"]; // wzrost w cm
+  //sprawdzenie  czy dane są poprawne
+  $BMI=$waga/($wzrost/100*$wzrost/100); //obliczenie wskaźnika BMI
+  //sprawdzanie przedziału obliczonego wskaźnika
+  switch ($BMI) {
+    case ($BMI < 15):
+      echo "Wskaźnik BMI wynosi: ".round($BMI)."<div>"."Anoreksja";
+      break;
+    case ($BMI >= 15 && $BMI <= 17.4):
+      echo "Wskaźnik BMI wynosi: ".round($BMI)."<div>"."Wychudzenie";
+      break;
+    case ($BMI >= 17.5 && $BMI <= 18.4):
+      echo "Wskaźnik BMI wynosi: ".round($BMI)."<div>"."Niedowaga";
+      break;
+    case ($BMI >= 18.5 && $BMI <= 24.9):
+      echo "Wskaźnik BMI wynosi: ".round($BMI)."<div>"."Waga prawidłowa";
+      break;
+    case ($BMI >= 25 && $BMI <= 29.9):
+      echo "Wskaźnik BMI wynosi: ".round($BMI)."<div>"."Nadwaga";
+      break;
+    case ($BMI >= 30 && $BMI <= 34.9):
+      echo "Wskaźnik BMI wynosi: ".round($BMI)."<div>"."OtyłośĆ pierwszego stopnia";
+      break;
+    case ($BMI >= 35 && $BMI <= 39.9):
+      echo "Wskaźnik BMI wynosi: ".round($BMI)."<div>"."OtyłośĆ drugiego stopnia";
+      break;
+    case ($BMI >= 40):
+      echo "Wskaźnik BMI wynosi: ".round($BMI)."<div>"."Otyłość trzeciego stopnia";
+      break;
   }
- elseif($BMI >= 15 && $BMI <= 17.4){
-     echo "Wskaźnik BMI wynosi: ".round($BMI)."<br>"."Wychudzenie";
-   }
- elseif($BMI >= 17.5 && $BMI <= 18.4){
-     echo "Wskaźnik BMI wynosi: ".round($BMI)."<br>"."Niedowaga";
-   }
- elseif($BMI >= 18.5 && $BMI <= 24.9){
-     echo "Wskaźnik BMI wynosi: ".round($BMI)."<br>"."Waga prawidłowa";
-   }
- elseif($BMI >= 25 && $BMI <= 29.9){
-     echo "Wskaźnik BMI wynosi: ".round($BMI)."<br>"."Nadwaga";
-   }
- elseif($BMI >= 30 && $BMI <= 34.9){
-     echo "Wskaźnik BMI wynosi: ".round($BMI)."<br>"."OtyłośĆ pierwszego stopnia";
-   }
- elseif($BMI >= 35 && $BMI <= 39.9){
-     echo "Wskaźnik BMI wynosi: ".round($BMI)."<br>"."OtyłośĆ drugiego stopnia";
-   }
- elseif($BMI >= 40){
-     echo "Wskaźnik BMI wynosi: ".round($BMI)."<br>"."Otyłość trzeciego stopnia";
-   }
 
-?>
+}
--- index.php
+++ index.php
@@ -2,14 +2,25 @@
 <html lang="pl">
   <head>
     <meta charset="utf-8">
-    <title>Kalkulator BMI</title>
+    <link rel="stylesheet" href="style.css" type="text/css"
+
   </head>
   <body>
+    <div id="container">
     <form action="bmi.php" method="post">
-      Podaj wagę w kilogramach <input type="integer" name="waga" required ><br/>
-      Podaj wzrost w centymetrach<input type="integer" name="wzrost"required ><br/>
-      <input type="submit" name="Wyślij"><br/>
+      Podaj wagę w kilogramach <input type="integer" name="waga" required >
+      Podaj wzrost w centymetrach<input type="integer" name="wzrost"required >
+      <input type="submit" value="Sprawdź" name="submit">
     </form>
-
   </body>
+  <?php
+  if (isset($_POST["submit"])){
+    if (isset($_POST["waga"]) && isset($_POST["wzrost"]) && $_POST["waga"] > 0 && $_POST["wzrost"] > 0)
+    {
+       calculateBMI();
+    } else {
+      echo "Nieprawidłowe dane!";
+    }
+  }
+  ?>
 </html>
